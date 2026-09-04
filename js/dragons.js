00cfa385ae5dcada250cb8596b7d7bd6ca9e127f
patty-tech <57613852+patty-tech@users.noreply.github.com>
2026-09-04 21:43:02 +0400

    Add starter dragon data

---
+++ js/dragons.js
@@ -0,0 +1,6 @@
+window.DRAGON_DATA = [
+  {id:'pyron',name:'Pyron',element:'Fire',basePower:17,mark:'P'},
+  {id:'tidera',name:'Tidera',element:'Water',basePower:15,mark:'T'},
+  {id:'mossfang',name:'Mossfang',element:'Nature',basePower:14,mark:'M'},
+  {id:'gravorn',name:'Gravorn',element:'Earth',basePower:18,mark:'G'}
+];
