36d820beaaec44368d4f58cc080a9f04d17e0fbd
patty-tech <57613852+patty-tech@users.noreply.github.com>
2026-09-04 21:53:28 +0400

    Upgrade dragon roster for Web v2

--- js/dragons.js
+++ js/dragons.js
@@ -1,6 +1,17 @@
 window.DRAGON_DATA = [
-  {id:'pyron',name:'Pyron',element:'Fire',basePower:17,mark:'P'},
-  {id:'tidera',name:'Tidera',element:'Water',basePower:15,mark:'T'},
-  {id:'mossfang',name:'Mossfang',element:'Nature',basePower:14,mark:'M'},
-  {id:'gravorn',name:'Gravorn',element:'Earth',basePower:18,mark:'G'}
+  {id:'pyron',name:'Pyron',elements:['Fire'],basePower:17,mark:'P',rarity:'Common'},
+  {id:'tidera',name:'Tidera',elements:['Water'],basePower:15,mark:'T',rarity:'Common'},
+  {id:'mossfang',name:'Mossfang',elements:['Nature'],basePower:14,mark:'M',rarity:'Common'},
+  {id:'gravorn',name:'Gravorn',elements:['Earth'],basePower:18,mark:'G',rarity:'Common'},
+  {id:'steamwing',name:'Steamwing',elements:['Fire','Water'],basePower:24,mark:'S',rarity:'Rare'},
+  {id:'bloomscale',name:'Bloomscale',elements:['Nature','Earth'],basePower:23,mark:'B',rarity:'Rare'},
+  {id:'cinderroot',name:'Cinderroot',elements:['Fire','Nature'],basePower:22,mark:'C',rarity:'Rare'},
+  {id:'mudfin',name:'Mudfin',elements:['Water','Earth'],basePower:22,mark:'U',rarity:'Rare'}
 ];
+
+window.BREEDING_RECIPES = {
+  'pyron+tidera':'steamwing','tidera+pyron':'steamwing',
+  'mossfang+gravorn':'bloomscale','gravorn+mossfang':'bloomscale',
+  'pyron+mossfang':'cinderroot','mossfang+pyron':'cinderroot',
+  'tidera+gravorn':'mudfin','gravorn+tidera':'mudfin'
+};
